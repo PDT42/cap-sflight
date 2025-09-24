a89b00f3cc847b4a0a634e97a0c5c48aa650eff2
I548646 <paul.erlenwein@sap.com>
2025-09-24 09:56:11 +0200

    fix: validation message

--- srv/travel-service.ts
+++ srv/travel-service.ts
@@ -34,7 +34,7 @@ export class TravelService extends cds.ApplicationService { init() {
   this.before ('SAVE', Travel, req => {
     const { BeginDate, EndDate } = req.data
     // @ts-ignore
-    if (BeginDate < today()) req.error ({code: 422, message: 'End Date must be after Begin Date.', target: 'in/EndDate', additionalTargets: ['in/BeginDate']})
+    if (BeginDate < today()) req.error ({code: 422, message: 'Begin Date must be in the future.', target: 'in/BeginDate'})
       // @ts-ignore
     if (BeginDate > EndDate) req.error ({code: 422, message: 'End Date must be after Begin Date.', target: 'in/EndDate', additionalTargets: ['in/BeginDate']})
   })
